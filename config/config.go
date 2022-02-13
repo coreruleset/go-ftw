d41e859a57f99ae4fdb40789285f2645b8fa2830
Felipe Zipitria <felipe.zipitria@owasp.org>
2022-02-13 11:24:27 -0300

    feat(override): adds feature for everriding host and port globally on all tests
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- config/config.go
+++ config/config.go
@@ -25,11 +25,13 @@ type FTWLogType struct {
 	TimeTruncate time.Duration `koanf:"timetruncate"`
 }
 
-// FTWTestOverride holds three lists:
+// FTWTestOverride holds four lists:
+//   Global allows you to override global parameters in tests. An example usage is if you want to change the `dest_addr` of all tests to point to an external IP or host.
 //   Ignore is for tests you want to ignore. It will still execute the test, but ignore the results. You should add a comment on why you ignore the test
 //   ForcePass is for tests you want to pass unconditionally. Test will be executed, and pass even when the test fails. You should add a comment on why you force pass the test
 //   ForceFail is for tests you want to fail unconditionally. Test will be executed, and fail even when the test passes. You should add a comment on why you force fail the test
 type FTWTestOverride struct {
+	Input     map[string]string `koanf:"input"`
 	Ignore    map[string]string `koanf:"ignore"`
 	ForcePass map[string]string `koanf:"forcepass"`
 	ForceFail map[string]string `koanf:"forcefail"`
