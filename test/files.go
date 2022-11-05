2693cf6323d0b0f9483a9cc901c51fd85ac2f430
Felipe Zipitria <felipe.zipitria@owasp.org>
2022-11-05 10:39:14 +0100

    fix: add examples on how to fix errors
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- test/files.go
+++ test/files.go
@@ -78,16 +78,29 @@ func describeYamlError(yamlError error) string {
 		return err.Error()
 	}
 	if matched {
-		return "This might refer to a \"status\" line being '200', where it should be '[200]'. " +
-			"The default \"status\" is a list now."
+		return "\nTip: This might refer to a \"status\" line being '200', where it should be '[200]'.\n" +
+			"The default \"status\" is a list now.\n" +
+			"A simple example would be like this:\n\n" +
+			"status: 403\n" +
+			"needs to be changed to:\n\n" +
+			"status: [403]\n\n"
 	}
 	matched, err = regexp.MatchString(`.*cannot unmarshal \[]interface {} into Go struct field FTWTest.Tests of type string.*`, yamlError.Error())
 	if err != nil {
 		return err.Error()
 	}
 	if matched {
-		return "This might refer to \"data\" on the test being a list of strings instead of a proper YAML multiline. " +
-			"To fix this, convert this \"data\" string list to a multiline YAML and this will be fixed. "
+		return "\nTip: This might refer to \"data\" on the test being a list of strings instead of a proper YAML multiline.\n" +
+			"To fix this, convert this \"data\" string list to a multiline YAML and this will be fixed.\n" +
+			"A simple example would be like this:\n\n" +
+			"data:\n" +
+			"  - 'Hello'\n" +
+			"  - 'World'\n" +
+			"can be expressed as:\n\n" +
+			"data: |\n" +
+			"  Hello\n" +
+			"  World\n\n" +
+			"You can also remove single/double quotes from beggining and end of text, they are not needed. See https://yaml-multiline.info/ for additional help.\n"
 	}
 
 	return "We do not have an extended explanation of this error."
