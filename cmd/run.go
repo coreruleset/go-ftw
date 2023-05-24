626edd08327c0df9a7f4794fcff79e01bd965d56
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-05-23 22:49:41 -0300

    feat: add waiting for http response

--- cmd/run.go
+++ cmd/run.go
@@ -7,11 +7,17 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/spf13/cobra"
+	"github.com/go-logr/zerologr"
+	"github.com/rs/zerolog/log"
+	"wait4x.dev/v2/waiter"
+
+	"wait4x.dev/v2/checker"
+	"wait4x.dev/v2/checker/http"
 
 	"github.com/coreruleset/go-ftw/output"
 	"github.com/coreruleset/go-ftw/runner"
 	"github.com/coreruleset/go-ftw/test"
+	"github.com/spf13/cobra"
 )
 
 // runCmd represents the run command
@@ -25,6 +31,7 @@ var runCmd = &cobra.Command{
 		include, _ := cmd.Flags().GetString("include")
 		id, _ := cmd.Flags().GetString("id")
 		dir, _ := cmd.Flags().GetString("dir")
+		outputFilename, _ := cmd.Flags().GetString("file")
 		showTime, _ := cmd.Flags().GetBool("time")
 		showOnlyFailed, _ := cmd.Flags().GetBool("show-failures-only")
 		wantedOutput, _ := cmd.Flags().GetString("output")
@@ -32,6 +39,17 @@ var runCmd = &cobra.Command{
 		readTimeout, _ := cmd.Flags().GetDuration("read-timeout")
 		maxMarkerRetries, _ := cmd.Flags().GetInt("max-marker-retries")
 		maxMarkerLogLines, _ := cmd.Flags().GetInt("max-marker-log-lines")
+		// wait4x flags
+		timeout, _ := cmd.Flags().GetDuration("wait-for-timeout")
+		interval, _ := cmd.Flags().GetDuration("wait-for-interval")
+		expectStatusCode, _ := cmd.Flags().GetInt("wait-for-expect-status-code")
+		expectBodyRegex, _ := cmd.Flags().GetString("wait-for-expect-body-regex")
+		expectBodyJSON, _ := cmd.Flags().GetString("wait-for-expect-body-json")
+		expectBodyXPath, _ := cmd.Flags().GetString("wait-for-expect-body-xpath")
+		expectHeader, _ := cmd.Flags().GetString("wait-for-expect-header")
+		connectionTimeout, _ := cmd.Flags().GetDuration("wait-for-connection-timeout")
+		insecureSkipTLSVerify, _ := cmd.Flags().GetBool("wait-for-insecure-skip-tls-verify")
+		noRedirect, _ := cmd.Flags().GetBool("wait-for-no-redirect")
 
 		if wantedOutput == "" {
 			wantedOutput = "normal"
@@ -66,8 +84,47 @@ var runCmd = &cobra.Command{
 			excludeRE = regexp.MustCompile(exclude)
 		}
 
-		//TODO: pass --file parameter to change this file
-		out := output.NewOutput(wantedOutput, os.Stdout)
+		// Add wait4x checkers
+		checkers := make([]checker.Checker, 0)
+		for _, arg := range args {
+			hc := http.New(arg,
+				http.WithExpectStatusCode(expectStatusCode),
+				http.WithExpectBodyRegex(expectBodyRegex),
+				http.WithExpectBodyJSON(expectBodyJSON),
+				http.WithExpectBodyXPath(expectBodyXPath),
+				http.WithExpectHeader(expectHeader),
+				http.WithTimeout(connectionTimeout),
+				http.WithInsecureSkipTLSVerify(insecureSkipTLSVerify),
+				http.WithNoRedirect(noRedirect),
+			)
+
+			checkers = append(checkers, hc)
+		}
+
+		if len(checkers) > 0 {
+			Logger := zerologr.New(&log.Logger)
+			waiterErr := waiter.WaitParallelContext(
+				cmd.Context(),
+				checkers,
+				waiter.WithTimeout(timeout),
+				waiter.WithInterval(interval),
+				waiter.WithLogger(Logger),
+			)
+			if waiterErr != nil {
+				return waiterErr
+			}
+		}
+		// use outputFile to write to file
+		var outputFile *os.File
+		if outputFilename == "" {
+			outputFile = os.Stdout
+		} else {
+			outputFile, err = os.Open(outputFilename)
+			if err != nil {
+				return err
+			}
+		}
+		out := output.NewOutput(wantedOutput, outputFile)
 		_ = out.Println("%s", out.Message("** Starting tests!"))
 
 		currentRun, err := runner.Run(cfg, tests, runner.RunnerConfig{
@@ -96,10 +153,21 @@ func init() {
 	runCmd.Flags().StringP("id", "", "", "(deprecated). Use --include matching your test only.")
 	runCmd.Flags().StringP("dir", "d", ".", "recursively find yaml tests in this directory")
 	runCmd.Flags().StringP("output", "o", "normal", "output type for ftw tests. \"normal\" is the default.")
+	runCmd.Flags().StringP("file", "f", "", "output file for ftw tests. \"os.Stdout\" is the default.")
 	runCmd.Flags().BoolP("time", "t", false, "show time spent per test")
 	runCmd.Flags().BoolP("show-failures-only", "", false, "shows only the results of failed tests")
 	runCmd.Flags().Duration("connect-timeout", 3*time.Second, "timeout for connecting to endpoints during test execution")
 	runCmd.Flags().Duration("read-timeout", 1*time.Second, "timeout for receiving responses during test execution")
 	runCmd.Flags().Int("max-marker-retries", 20, "maximum number of times the search for log markers will be repeated.\nEach time an additional request is sent to the web server, eventually forcing the log to be flushed")
 	runCmd.Flags().Int("max-marker-log-lines", 500, "maximum number of lines to search for a marker before aborting")
+	runCmd.Flags().Duration("wait-for-interval", 1*time.Second, "Interval time between each loop.")
+	runCmd.Flags().Duration("wait-for-timeout", 10*time.Second, "Timeout is the maximum amount of time that Wait4X will wait for a checking operation, 0 is unlimited.")
+	runCmd.Flags().Int("wait-for-expect-status-code", 0, "Expect response code e.g. 200, 204, ... .")
+	runCmd.Flags().String("wait-for-expect-body-regex", "", "Expect response body pattern.")
+	runCmd.Flags().String("wait-for-expect-body-json", "", "Expect response body JSON pattern.")
+	runCmd.Flags().String("wait-for-expect-body-xpath", "", "Expect response body XPath pattern.")
+	runCmd.Flags().String("wait-for-expect-header", "", "Expect response header pattern.")
+	runCmd.Flags().Duration("wait-for-connection-timeout", http.DefaultConnectionTimeout, "Http connection timeout, The timeout includes connection time, any redirects, and reading the response body.")
+	runCmd.Flags().Bool("wait-for-insecure-skip-tls-verify", http.DefaultInsecureSkipTLSVerify, "Skips tls certificate checks for the HTTPS request.")
+	runCmd.Flags().Bool("wait-for-no-redirect", http.DefaultNoRedirect, "Do not follow HTTP 3xx redirects.")
 }
