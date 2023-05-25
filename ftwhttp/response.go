d88cbb0f547d3376a468526c56314c1180c5bec4
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-05-25 10:47:52 -0300

    fix: remove not needed BodyAsString
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- ftwhttp/response.go
+++ ftwhttp/response.go
@@ -1,19 +1,6 @@
 package ftwhttp
 
-import (
-	"io"
-)
-
 // GetFullResponse gives the full response as string, or nil if there was some error
 func (r *Response) GetFullResponse() string {
 	return string(r.RAW)
-}
-
-// GetBodyAsString gives the response body as string, or nil if there was some error
-func (r *Response) GetBodyAsString() string {
-	body, err := io.ReadAll(r.Parsed.Body)
-	if err != nil {
-		return ""
-	}
-	return string(body)
 }
