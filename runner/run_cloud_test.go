5451191541a2fa526662d6434d8d778ab2bebd6b
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-06-04 12:30:25 -0300

    feat: add last runner/ tests
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

---
+++ runner/run_cloud_test.go
@@ -0,0 +1,115 @@
+package runner
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"testing"
+	"text/template"
+
+	"github.com/coreruleset/go-ftw/config"
+	"github.com/coreruleset/go-ftw/ftwhttp"
+	"github.com/coreruleset/go-ftw/output"
+	"github.com/coreruleset/go-ftw/test"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/suite"
+)
+
+type runCloudTestSuite struct {
+	suite.Suite
+	cfg          *config.FTWConfiguration
+	ftwTests     []test.FTWTest
+	out          *output.Output
+	ts           *httptest.Server
+	dest         *ftwhttp.Destination
+	tempFileName string
+}
+
+func TestRunCloudTestSuite(t *testing.T) {
+	suite.Run(t, new(runCloudTestSuite))
+}
+
+func (s *runCloudTestSuite) SetupTest() {
+	s.newTestCloudServer()
+	s.out = output.NewOutput("normal", os.Stdout)
+}
+
+func (s *runCloudTestSuite) TearDownTest() {
+	s.ts.Close()
+}
+
+func (s *runCloudTestSuite) BeforeTest(_ string, name string) {
+	var err error
+
+	// if we have a destination for this test, use it
+	// else use the default destination
+	if s.dest == nil {
+		s.dest, err = ftwhttp.DestinationFromString(destinationMap[name])
+		s.NoError(err)
+	}
+
+	log.Info().Msgf("Using port %d and addr '%s'", s.dest.Port, s.dest.DestAddr)
+
+	// set up variables for template
+	vars := map[string]interface{}{
+		"TestPort": s.dest.Port,
+		"TestAddr": s.dest.DestAddr,
+	}
+
+	s.cfg = config.NewCloudConfig()
+	// get tests template from file
+	tmpl, err := template.ParseFiles(fmt.Sprintf("testdata/%s.yaml", name))
+	s.NoError(err)
+	// create a temporary file to hold the test
+	testFileContents, err := os.CreateTemp("testdata", "mock-test-*.yaml")
+	s.NoError(err, "cannot create temporary file")
+	err = tmpl.Execute(testFileContents, vars)
+	s.NoError(err, "cannot execute template")
+	// get tests from file
+	s.ftwTests, err = test.GetTestsFromFiles(testFileContents.Name())
+	s.NoError(err, "cannot get tests from file")
+	// save the name of the temporary file so we can delete it later
+	s.tempFileName = testFileContents.Name()
+}
+
+func (s *runCloudTestSuite) AfterTest(_ string, _ string) {
+	if s.tempFileName != "" {
+		err := os.Remove(s.tempFileName)
+		s.NoError(err, "cannot remove test file")
+		s.tempFileName = ""
+	}
+}
+
+// Error checking omitted for brevity
+func (s *runCloudTestSuite) newTestCloudServer() {
+	var err error
+
+	s.ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		statusCode := http.StatusOK
+		if r.URL.Path != "/" {
+			statusCode, err = strconv.Atoi(r.URL.Path[1:])
+			if err != nil {
+				statusCode = http.StatusBadRequest
+			}
+			log.Debug().Msgf("Mock cloud server returning status code: %d", statusCode)
+		}
+		w.WriteHeader(statusCode)
+		_, _ = w.Write([]byte("Hello, client"))
+	}))
+
+	s.dest, err = ftwhttp.DestinationFromString((s.ts).URL)
+	s.Require().NoError(err, "cannot get destination from string")
+}
+
+func (s *runCloudTestSuite) TestCloudRun() {
+	s.Run("don't show time and execute all", func() {
+		res, err := Run(s.cfg, s.ftwTests, RunnerConfig{
+			ShowTime: true,
+			Output:   output.Quiet,
+		}, s.out)
+		s.NoError(err)
+		s.Equalf(res.Stats.TotalFailed(), 0, "Oops, %d tests failed to run!", res.Stats.TotalFailed())
+	})
+}
