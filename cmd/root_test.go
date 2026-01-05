d87466aedc2df3a0144cdaa1dceea27ccffff316
Matteo Pace <pace.matteo96@gmail.com>
2026-01-05 22:45:05 +0100

    fix: flags not persisted into CommandContext

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -10,13 +10,15 @@ import (
 	run "github.com/coreruleset/go-ftw/v2/cmd/run"
 	"github.com/coreruleset/go-ftw/v2/config"
 	"github.com/coreruleset/go-ftw/v2/utils"
+	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/suite"
 )
 
 type rootCmdTestSuite struct {
 	suite.Suite
-	rootCmd *cobra.Command
+	rootCmd    *cobra.Command
+	cmdContext *internal.CommandContext
 }
 
 func TestRootTestSuite(t *testing.T) {
@@ -24,10 +26,11 @@ func TestRootTestSuite(t *testing.T) {
 }
 
 func (s *rootCmdTestSuite) SetupTest() {
-	s.rootCmd = NewRootCommand()
+	s.cmdContext = internal.NewCommandContext()
+	s.rootCmd = NewRootCommand(s.cmdContext)
 }
 func (s *rootCmdTestSuite) TestRootCommand() {
-	rootCmd := NewRootCommand()
+	rootCmd := NewRootCommand(internal.NewCommandContext())
 	rootCmd.SetArgs([]string{"help"})
 	err := Execute("v1.0.0")
 	s.Require().NoError(err)
@@ -38,29 +41,29 @@ func (s *rootCmdTestSuite) TestFlags() {
 	s.Require().NoError(err)
 	overridesFile, err := utils.CreateTempFile(s.T().TempDir(), "overrides")
 	s.Require().NoError(err)
+
 	s.rootCmd.SetArgs([]string{
 		"run",
 		"--" + configFlagName, configFile,
 		"--" + debugFlagName,
 		"--" + overridesFlagName, overridesFile,
 		"--" + traceFlagName,
+		"--" + cloudFlagName,
 	})
-	cmdContext := internal.NewCommandContext()
-	cmdContext.Configuration = config.NewDefaultConfig()
-	s.rootCmd.AddCommand(run.New(cmdContext))
-	cmd, _ := s.rootCmd.ExecuteC()
+	s.rootCmd.AddCommand(run.New(s.cmdContext))
+	s.cmdContext.Configuration = config.NewDefaultConfig()
 
-	config, err := cmd.Flags().GetString(configFlagName)
-	s.NoError(err)
-	debug, err := cmd.Flags().GetBool(debugFlagName)
-	s.NoError(err)
-	overrides, err := cmd.Flags().GetString(overridesFlagName)
-	s.NoError(err)
-	trace, err := cmd.Flags().GetBool(traceFlagName)
-	s.NoError(err)
+	// Reset log level to info before test
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
-	s.Equal(configFile, config)
-	s.True(debug)
-	s.Equal(overridesFile, overrides)
-	s.True(trace)
+	_, err = s.rootCmd.ExecuteC()
+	s.Require().NoError(err)
+	// Validate that flags populate command context
+	s.Equal(configFile, s.cmdContext.ConfigurationFileName)
+	s.Equal(overridesFile, s.cmdContext.OverridesFileName)
+	s.True(s.cmdContext.Debug)
+	s.True(s.cmdContext.Trace)
+	s.True(s.cmdContext.CloudMode)
+	// Validate that log level is set correctly (trace takes precedence over debug)
+	s.Equal(zerolog.TraceLevel, zerolog.GlobalLevel())
 }
